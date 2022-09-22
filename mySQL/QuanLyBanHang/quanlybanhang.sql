18d4f9ae322c59bac9f1030284be504af8f5794c
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-09-22 22:28:54 +0700

    Quan Ly Ban Hang

---
+++ mySQL/QuanLyBanHang/quanlybanhang.sql
@@ -0,0 +1,36 @@
+create database QuanLyBanHang;
+
+use QuanLyBanHang;
+
+create table customer
+(
+	cID Varchar(10) not null primary key,
+    cName varchar(50) not null,
+    cAge int not null
+);
+
+create table Orders
+(
+	oID varchar(10) 	not null primary key,
+    cID varchar(10) 	not null,
+    oDate date 			not null,
+    oTtalPrice double 	not null, 
+    foreign key (cID) references customer(cID)
+);
+
+create table Product
+(
+	pID varchar(10) 	not null primary key,
+    pName varchar(20) 	not null,
+    pPrice double 		not null
+);
+
+create table OrderDetail
+(
+	oID varchar(10) not null,
+    pID varchar(10) not null,
+    odQTY int 		not null,
+    primary key (oID,pID),
+    foreign key (oID) references Orders(oID),
+    foreign key (pID) references Product(pID)
+);
