5081e0262128ba2284e8248a8107304acb43c338
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-10-07 08:50:07 +0700

    tu dien tieng anh

---
+++ Servlet/simple_dictionary/src/main/java/com/khanhhoang/simple_dictionary/TranslationServlet.java
@@ -0,0 +1,40 @@
+package com.khanhhoang.simple_dictionary;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "TranslationServlet", value = "/translate")
+public class TranslationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chao");
+        dictionary.put("how", "The nao");
+        dictionary.put("book", "Quyen vo");
+        dictionary.put("computer", "May tinh");
+
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if(result != null){
+            writer.println("Word: " + search);
+            writer.println("<br>");
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+
+        writer.println("</html>");
+    }
+}
